eb7b82f5af09adf056b99fb0c8b5374d9dcac0e1
ishuu7 <narendramanglani04@gmail.com>
2020-05-22 16:50:28 +0530

    22MAY2020

---
+++ Range-Queries/Range-Sum-Queries-2.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define loop(i,a,b) for(int i = (int)a ; i < (int)b ; i++)
+#define rtloop(i,a,b) for(int i = (int)a; i >= (int)b; i--)
+#define pb push_back
+#define mp make_pair
+#define yo(a) if(local == 'L'){cerr <<#a <<" = " << a <<"\n";}
+#define COOL ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+//char local = '0';
+
+const int N = 2e5 + 1;
+
+int a[N], t[4*N];
+
+void build(int v, int tl, int tr) {
+    if(tl == tr){
+        t[v] = a[tl];
+        return;
+    }
+    int tm = (tr + tl) / 2;
+    build(v*2, tl, tm);
+    build(v*2 + 1, tm+1, tr);
+    t[v] = (t[2*v] + t[2*v + 1]);
+}
+
+int sum(int v, int tl, int tr, int l, int r) {
+    if(tl > r || tr < l)
+        return 0;
+    if(tl >= l && tr <= r)
+        return t[v];
+    int tm = (tl + tr) / 2;
+    return (sum(v*2, tl, tm, l, r) + sum(v*2 + 1, tm+1, tr, l, r));
+}
+
+void update(int v, int tl, int tr, int pos, int newVal) {
+    if(tr == tl) {
+        t[v] = newVal;
+        return;
+    }
+    int tm = (tl + tr) / 2;
+    if(pos <= tm)
+        update(v*2, tl, tm, pos, newVal);
+    else
+        update(v*2 + 1, tm+1, tr, pos, newVal);
+    t[v] = t[v*2] + t[v*2 + 1];
+}
+
+
+int32_t main() {
+    COOL
+    #ifdef L
+        //local = 'L';
+        freopen("in.txt", "r", stdin);
+        freopen("op.txt", "w", stdout);
+    #endif
+    int n, q;
+    cin >> n >> q;
+    loop(i, 1, n+1)cin >> a[i];
+    build(1, 1, n); 
+    int l1, r1, x;
+    while(q--) {
+        cin >> x >> l1 >> r1;
+        if(x == 2)
+            cout << sum(1, 1, n, l1, r1) << "\n";
+        else 
+            update(1, 1, n, l1, r1);
+    }
+    return 0;
+}
